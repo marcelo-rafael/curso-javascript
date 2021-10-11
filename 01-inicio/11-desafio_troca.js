7a126a19137cb0fa7c0b89e303b70296a4fbe4b8
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-10-10 23:39:49 -0300

    iniciando com programação

---
+++ 01-inicio/11-desafio_troca.js
@@ -0,0 +1,15 @@
+let a = 7
+let b = 100
+
+// depois da troca... a = 94 e b = 7
+console.log('antes');
+console.log(a)
+console.log(b)
+
+let temp = a
+a = b
+b = temp
+
+console.log('depois');
+console.log(a)
+console.log(b)
