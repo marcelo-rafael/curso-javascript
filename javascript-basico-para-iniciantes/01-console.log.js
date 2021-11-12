973732c936cd7d31f840d816730d03ab47fd5d0b
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-11-11 21:58:38 -0300

    console.log

---
+++ javascript-basico-para-iniciantes/01-console.log.js
@@ -0,0 +1,9 @@
+console.log("Marcelo") // String = texto
+console.log('Marcelo')
+console.log(`Marcelo`)
+
+console.log(12345)
+
+// Exercícios
+
+console.log('Meu nome é "Marcelo". Estou aprendendo JavaScript às', 10, 'da manhã')
