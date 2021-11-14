6c7ae384ba5f426cfdab32ce6e446f1729d1f620
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-11-14 20:11:56 -0300

    Tipos de dados primitivos

---
+++ 01-javascript-basico-para-iniciantes/06-tipos-de-dados-primitivos.js
@@ -0,0 +1,9 @@
+const nome = 'Marcelo' // string
+const nome1 = "Marcelo" // string
+const nome 2= `Marcelo` // string
+const num1 = 10 // number
+const num2 = 10.52 // number
+let nomeAluno // undefined -> não aponta para local nenhum da memória
+const sobreNomeAluno = null // Nulo -> não aponta para local nenhum da memória
+const aprovado = false // Boolean = true ou false (lógico)
+
