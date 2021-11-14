85360f60137b4bbbea88c4e6d5c0b8dc009bfd00
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-11-14 19:36:08 -0300

    Constantes com const

---
+++ 01-javascript-basico-para-iniciantes/04-Constantes com const.js
@@ -0,0 +1,17 @@
+// Não podemos criar constantes com palavras reservadas
+// Constantes precisam ter nomes significativos
+// Não pode começar o nome de uma constantes com número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não pode modificar o valor de uma constante
+// NÃO UTILIZAR VAR, UTILIZE CONST.
+
+const nome = 'Marcelo'
+
+console.log(nome)
+
+const primeiroNumero = 5
+const segundoNumero = 10
+const resultado = primeiroNumero * segundoNumero
+console.log(resultado)
