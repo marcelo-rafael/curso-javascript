1db968081e76d51d43d70c5f3c4e26a817ed29e8
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-12-12 14:48:06 -0300

    Mais sobre numbers

---
+++ 01-javascript-basico-para-iniciantes/12-mais-sobre-numbers.js
@@ -0,0 +1,12 @@
+let num1 = 10.25 // number
+let num2 = 2.5 // number
+
+
+// console.log(num1.toString() + num2) // converter numero para string
+// num1 = num1.toString()
+// console.log(typeof num1)
+// console.log(num1.toString(2))
+// console.log(num1.toFixed(2))
+// console.log(Number.isInteger(num1))
+// let temp = num1 * 'Olá'
+// console.log(Number.isNaN(temp))
