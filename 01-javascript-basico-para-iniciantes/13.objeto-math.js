34f868c50c175bd52c6e7bd611b52d25bcc21073
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-12-12 15:26:19 -0300

    Objeto Math

---
+++ 01-javascript-basico-para-iniciantes/13.objeto-math.js
@@ -0,0 +1,14 @@
+let num1 =16
+
+// console.log(num1 ** 0.5) // pegar raiz quadrada de algum número
+
+// let num2 = Math.floor(num1) // Math.floor arredonda para baixo
+// let num2 = Math.ceil(num1) // Math.floor arredonda para cima 
+// let num2 = Math.round(num1) // Math.floor arredonda para o número mais proximo 
+// console.log(num2)
+// console.log(Math.max(1, 2, 3, 4, 5, -10, -50, 1500)) // trás o maior número da sequência
+// console.log(Math.min(1, 2, 3, 4, 5, -10, -50, 1500)) // trás o menor número da sequência
+// console.log(Math.random()) // retorna um número entre 0 e 10
+
+// console.log(Math.pow(2, 10)) // exponenciação
+// console.log(2 ** 10) // exponenciação
