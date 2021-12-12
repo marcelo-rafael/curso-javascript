fca457b92539cb6de0fd812717b41d42242d739a
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-12-12 17:15:24 -0300

    Arrays basico

---
+++ 01-javascript-basico-para-iniciantes/15-Arrays-basico.js
@@ -0,0 +1,39 @@
+const alunos = ['Luiz', 'Maria', 'João']
+
+// console.log(typeof alunos)
+// console.log(alunos instanceof Array)
+
+
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+
+// console.log(alunos.slice(0, -2)) //
+
+// delete alunos[1] // remover um elemento do array junto com o indice
+// console.log(alunos[1])
+// console.log(alunos)
+
+// const removido = alunos.pop() // remove o último  elemento do array
+// console.log(removido)
+// console.log(alunos)
+
+// const removido = alunos.shift() // remove o primeiro elemento do array
+// console.log(removido)
+// console.log(alunos)
+
+
+// alunos.unshift('Fábio') // Adiciona no começo
+// alunos.unshift('Luiza') // Adiciona no começo
+
+// alunos.push('Luiza')// Adiciona no fim
+// alunos.push('Fábio')// Adiciona no fim
+
+// alunos[alunos.length] = 'Luiza' // Adiciona no fim
+// alunos[alunos.length] = 'Fábio'
+
+// alunos[0] = 'Eduardo' // Alterar
+// alunos[3] = 'Luiza' // Adicionando
+
+// console.log(alunos)
+// console.log(alunos[0])
+// console.log(alunos[2])
