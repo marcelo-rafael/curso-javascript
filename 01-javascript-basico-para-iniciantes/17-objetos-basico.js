eac4bfd3bb7c1865a8a3a84f492e41ede0c968e1
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-12-12 19:00:01 -0300

    Objetos basicos

---
+++ 01-javascript-basico-para-iniciantes/17-objetos-basico.js
@@ -0,0 +1,39 @@
+// const pessoa1 = {
+//   nome: 'Marcelo',
+//   sobrenome: 'Rafael',
+//   idade: 31
+// }
+
+// const pessoa2 = {
+//   nome: 'Ingrid',
+//   sobrenome: 'Queiroz',
+//   idade: 25
+// }
+
+// console.log(pessoa1.nome, pessoa2.nome)
+
+// function criaPessoa(nome, sobrenome, idade) {
+//   return {
+//     nome: nome,
+//     sobrenome: sobrenome,
+//     idade: idade
+//   }
+// }
+// const pessoa1 = criaPessoa('Marcelo', 'Rafael', 31)
+// const pessoa2 = criaPessoa('Marcio', 'Rafael', 31)
+// const pessoa3 = criaPessoa('Marlon', 'Rafael', 20)
+// const pessoa4 = criaPessoa('Maicon', 'Rafael', 22)
+
+// console.log(pessoa1.nome, pessoa2.nome, pessoa3.nome, pessoa4.nome)
+
+// const pessoa1 =  {
+//   nome: 'Marcelo',
+//   sobrenome: 'Rafael',
+//   idade: 31,
+
+//   fala() {
+//     console.log(`${this.nome} ${this.sobrenome} está falando oi...`)
+//   }
+// }
+
+// pessoa1.fala()
