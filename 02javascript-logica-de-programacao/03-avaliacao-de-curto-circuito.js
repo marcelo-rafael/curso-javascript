1020b146b532947621923996bcfe8c24456276a8
Marcelo Rafael Gonçalves <marcelo.rafael.goncalves@gmail.com>
2021-12-13 15:50:59 -0300

    Avaliação de curto circuito

---
+++ 02javascript-logica-de-programacao/03-avaliacao-de-curto-circuito.js
@@ -0,0 +1,14 @@
+/*
+ * && -> false && true -> false "o valor mesmo"
+ * || -> true || false -> vai retornar o valor verdadeiro
+ */
+
+/*
+ * FALSY
+ * false
+ * 0 
+ * '' "" ``
+ * null / undefined
+ * NaN
+ */
+
